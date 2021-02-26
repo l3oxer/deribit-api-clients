e830d317ebf90ca400fb879bb7429fcc8fccff66
l3oxer <33934681+l3oxer@users.noreply.github.com>
2021-02-26 14:28:48 +0100

    save

--- csharp/BotUI/Form1.Designer.cs
+++ csharp/BotUI/Form1.Designer.cs
@@ -23,35 +23,148 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.testConnectionButton = new System.Windows.Forms.Button();
+            this.authButton = new System.Windows.Forms.Button();
+            this.refreshTokenTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.createTestBuyOrder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // testConnectionButton
             // 
-            this.button1.Location = new System.Drawing.Point(348, 350);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.testConnectionButton.Location = new System.Drawing.Point(75, 104);
+            this.testConnectionButton.Name = "testConnectionButton";
+            this.testConnectionButton.Size = new System.Drawing.Size(75, 23);
+            this.testConnectionButton.TabIndex = 0;
+            this.testConnectionButton.Text = "Test Connection";
+            this.testConnectionButton.UseVisualStyleBackColor = true;
+            this.testConnectionButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // authButton
+            // 
+            this.authButton.Location = new System.Drawing.Point(75, 163);
+            this.authButton.Name = "authButton";
+            this.authButton.Size = new System.Drawing.Size(75, 23);
+            this.authButton.TabIndex = 1;
+            this.authButton.Text = "Auth";
+            this.authButton.UseVisualStyleBackColor = true;
+            this.authButton.Click += new System.EventHandler(this.authButton_Click);
+            // 
+            // refreshTokenTimer
+            // 
+            this.refreshTokenTimer.Interval = 600000;
+            this.refreshTokenTimer.Tick += new System.EventHandler(this.refreshTokenTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Time Untill Token Exp:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(142, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Price,
+            this.Type});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 298);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(786, 150);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "price";
+            this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
+            // 
+            // Type
+            // 
+            this.Type.HeaderText = "Type";
+            this.Type.Name = "Type";
+            this.Type.ReadOnly = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 268);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Orders";
+            // 
+            // createTestBuyOrder
+            // 
+            this.createTestBuyOrder.Location = new System.Drawing.Point(406, 163);
+            this.createTestBuyOrder.Name = "createTestBuyOrder";
+            this.createTestBuyOrder.Size = new System.Drawing.Size(75, 23);
+            this.createTestBuyOrder.TabIndex = 6;
+            this.createTestBuyOrder.Text = "CreateTestBuyOrder";
+            this.createTestBuyOrder.UseVisualStyleBackColor = true;
+            this.createTestBuyOrder.Click += new System.EventHandler(this.createTestBuyOrder_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.createTestBuyOrder);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.authButton);
+            this.Controls.Add(this.testConnectionButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button testConnectionButton;
+        private System.Windows.Forms.Button authButton;
+        private System.Windows.Forms.Timer refreshTokenTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button createTestBuyOrder;
     }
 }
 
--- csharp/BotUI/Form1.cs
+++ csharp/BotUI/Form1.cs
@@ -13,9 +13,22 @@ using Org.OpenAPITools.Api;
 using Org.OpenAPITools.Client;
 using Org.OpenAPITools.Model;
 using System.Net;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace BotUI {
     public partial class Form1 : Form {
+
+
+        private AuthenticationApi authApiInstance;
+        private PublicApi publicApiInstance;
+        private PrivateApi privateApiInstance;
+        private string accessToken = string.Empty;
+        private string refreshToken = string.Empty;
+
+
         public Form1() {
             InitializeComponent();
             ServicePointManager.Expect100Continue = true;
@@ -23,36 +36,14 @@ namespace BotUI {
         }
 
         private void button1_Click(object sender, EventArgs e) {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e) {
-
-            Configuration.Default.BasePath = "https://www.deribit.com/api/v2";
-            // Configure HTTP basic authorization: bearerAuth
-            Configuration.Default.Username = "l3oxergames@gmail.com";
-            Configuration.Default.Password = "@Aa909090";
-            Configuration.Default.AccessToken = "dFDXK2tOnwFfVWwhlGKOg3r5KDJy0eQnHWzTKMfO9wY";
-            
-
-
-
-
-            var accountApiInstance = new AccountManagementApi(Configuration.Default);
-            var sid = 56;  // int? | The user id for the subaccount
-
-            
-            //var apiInstance = new AuthenticationApi(Configuration.Default);
-           
-           
-
             try {
                 // Change the user name for a subaccount
-                
-               var apiInstance = new PublicApi(Configuration.Default);
+
+                var apiInstance = new PublicApi(Configuration.Default);
                 var expectedResult = "";  // string | The value \"exception\" will trigger an error response. This may be useful for testing wrapper libraries. (optional) 
 
                 Object result = apiInstance.PublicTestGet(expectedResult);
+                
                 MessageBox.Show(result.ToString());
             } catch (ApiException ex) {
                 MessageBox.Show("Exception whenapiInstance.PublicTestGet: " + ex.Message);
@@ -60,5 +51,232 @@ namespace BotUI {
                 MessageBox.Show(ex.StackTrace);
             }
         }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+            Configuration.Default.BasePath = "https://test.deribit.com/api/v2";
+            // Configure HTTP basic authorization: bearerAuth
+            Configuration.Default.Username = "l3oxergames@gmail.com";
+            Configuration.Default.Password = "@Aa90909090";
+            
+            //Configuration.Default.AccessToken = "gjF0ypJxao-8hmycbfgemBCjTtRnR9MMLxFxSkVoMyA";
+
+            authApiInstance = new AuthenticationApi(Configuration.Default);
+            privateApiInstance = new PrivateApi(Configuration.Default);
+
     }
+
+        private void authButton_Click(object sender, EventArgs e) {
+            
+            var grantType = "client_credentials";  // string | Method of authentication
+            var clientId = "yWELzn_u";  // string | Required for grant type `client_credentials` and `client_signature`
+            var clientSecret = "gjF0ypJxao-8hmycbfgemBCjTtRnR9MMLxFxSkVoMyA";  // string | Required for grant type `client_credentials`
+            var scope = "session:box trade:read_write";  // string | Describes type of the access for assigned token, possible values: `connection`, `session`, `session:name`, `trade:[read, read_write, none]`, `wallet:[read, read_write, none]`, `account:[read, read_write, none]`, `expires:NUMBER` (token will expire after `NUMBER` of seconds).</BR></BR> **NOTICE:** Depending on choosing an authentication method (```grant type```) some scopes could be narrowed by the server. e.g. when ```grant_type = client_credentials``` and ```scope = wallet:read_write``` it's modified by the server as ```scope = wallet:read``` (optional) 
+
+            try {
+                // Authenticate
+                Object rootResponse = authApiInstance.PublicAuthGet(grantType, Configuration.Default.Username,Configuration.Default.Password, clientId, clientSecret, string.Empty, string.Empty, string.Empty, string.Empty,string.Empty, scope:scope);
+                MessageBox.Show(rootResponse.ToString());
+                var settings = new JsonSerializerSettings {
+                    Converters =
+                            {
+                                new ForeignJsonNetContainerConverter(), new ForeignJsonNetValueConverter()
+                            },
+                };
+                settings.Formatting = Formatting.Indented;
+                RootAuthApi rootResponseObj = JsonConvert.DeserializeObject<RootAuthApi>(rootResponse.ToString(), settings);
+                //Clipboard.SetText(rootResponse.ToString());
+                AuthResult authRes= rootResponseObj.Result;
+                refreshToken = authRes.RefreshToken;
+                Configuration.Default.AccessToken = accessToken = authRes.AccessToken.Trim();
+                refreshTokenTimer.Enabled = true;
+            } catch (ApiException ex) {
+                MessageBox.Show("Exception when calling AuthenticationApi.PublicAuthGet: " + ex.Message);
+                MessageBox.Show("Status Code: " + ex.ErrorCode);
+                MessageBox.Show(ex.StackTrace);
+            }
+        }
+
+        private void refreshTokenTimer_Tick(object sender, EventArgs e) {
+            var grantType = "refresh_token";  // string | Method of authentication
+
+            try {
+                // Authenticate
+                Object rootResponse = authApiInstance.PublicAuthGet(grantType, string.Empty, string.Empty, string.Empty, string.Empty, refreshToken,string.Empty, string.Empty, string.Empty, string.Empty);
+                MessageBox.Show(rootResponse.ToString());
+                var settings = new JsonSerializerSettings {
+                    Converters =
+                            {
+                                new ForeignJsonNetContainerConverter(), new ForeignJsonNetValueConverter()
+                            },
+                };
+                settings.Formatting = Formatting.Indented;
+                RootAuthApi rootResponseObj = JsonConvert.DeserializeObject<RootAuthApi>(rootResponse.ToString(), settings);
+                //Clipboard.SetText(rootResponse.ToString());
+                AuthResult authRes = rootResponseObj.Result;
+                refreshToken = authRes.RefreshToken;
+                Configuration.Default.AccessToken = accessToken = authRes.AccessToken;
+
+            } catch (ApiException ex) {
+                MessageBox.Show("Exception when calling AuthenticationApi.PublicAuthGet: " + ex.Message);
+                MessageBox.Show("Status Code: " + ex.ErrorCode);
+                MessageBox.Show(ex.StackTrace);
+            }
+        }
+
+        private void createTestBuyOrder_Click(object sender, EventArgs e) {
+        ;
+
+            //ApiClient apiclient = new ApiClient(Configuration.Default);
+            Configuration.Default.AddDefaultHeader("Access-Control-Allow-Origin", "*");
+            Configuration.Default.AddDefaultHeader("Authorization", "Bearer " + accessToken);
+            var apiInstance = new PrivateApi(Configuration.Default);
+            var instrumentName = "BTC-PERPETUAL";  // string | Instrument name
+            var amount = 500.0M;  // decimal? | It represents the requested order size. For perpetual and futures the amount is in USD units, for options it is amount of corresponding cryptocurrency contracts, e.g., BTC or ETH
+            var type = "limit";  // string | The order type, default: `\"limit\"` (optional) 
+            var label = "Test Buy Order by Bot";  // string | user defined label for the order (maximum 32 characters) (optional) 
+            var price = 45000.0M;  // decimal? | <p>The order price in base currency (Only for limit and stop_limit orders)</p> <p>When adding order with advanced=usd, the field price should be the option price value in USD.</p> <p>When adding order with advanced=implv, the field price should be a value of implied volatility in percentages. For example,  price=100, means implied volatility of 100%</p> (optional) 
+           // var timeInForce = timeInForce_example;  // string | <p>Specifies how long the order remains in effect. Default `\"good_til_cancelled\"`</p> <ul> <li>`\"good_til_cancelled\"` - unfilled order remains in order book until cancelled</li> <li>`\"fill_or_kill\"` - execute a transaction immediately and completely or not at all</li> <li>`\"immediate_or_cancel\"` - execute a transaction immediately, and any portion of the order that cannot be immediately filled is cancelled</li> </ul> (optional)  (default to good_til_cancelled)
+           // var maxShow = 8.14;  // decimal? | Maximum amount within an order to be shown to other customers, `0` for invisible order (optional)  (default to 1M)
+           // var postOnly = true;  // bool? | <p>If true, the order is considered post-only. If the new price would cause the order to be filled immediately (as taker), the price will be changed to be just below the bid.</p> <p>Only valid in combination with time_in_force=`\"good_til_cancelled\"`</p> (optional)  (default to true)
+           // var reduceOnly = true;  // bool? | If `true`, the order is considered reduce-only which is intended to only reduce a current position (optional)  (default to false)
+            var stopPrice = 45000.0M;  // decimal? | Stop price, required for stop limit orders (Only for stop orders) (optional) 
+          //  var trigger = trigger_example;  // string | Defines trigger type, required for `\"stop_limit\"` order type (optional) 
+          //  var advanced = advanced_example;  // string | Advanced option order type. (Only for options) (optional) 
+
+            try {
+                // Places a buy order for an instrument.
+                //privateApiInstance.Configuration.AccessToken = Configuration.Default.AccessToken;
+                MessageBox.Show("access token: " + privateApiInstance.Configuration.AccessToken);
+              //  MessageBox.Show(privateApiInstance.GetBasePath());
+                
+                foreach (string key in  Configuration.Default.DefaultHeader.Keys) {
+                  if(  privateApiInstance.DefaultHeader().TryGetValue(key, out string value)) {
+                        MessageBox.Show("key: " + key + " value: " + value);
+                    }
+                }
+                //   Configuration.Default.AddDefaultHeader("authorization", "bearer " + accessToken); ;
+               
+             //  apiInstance.AddDefaultHeader("Access-Control-Allow-Origin","*");
+                //  privateApiInstance.AddDefaultHeader("scope", "trade:read_write");
+                //ApiClient.Default.con = Configuration;
+              //  MessageBox.Show(ApiClient.Default.Configuration.AccessToken);
+                Object result = apiInstance.PrivateBuyGetWithHttpInfo(instrumentName, amount, type, label, price, string.Empty, null, null,null, stopPrice, string.Empty, string.Empty);
+                MessageBox.Show(result.ToString());
+                Clipboard.SetText(result.ToString());
+            } catch (ApiException ex) {
+              
+                MessageBox.Show("Exception when calling PrivateApi.PrivateBuyGet: " + ex.Message);
+                MessageBox.Show("Status Code: " + ex.ErrorCode);
+                MessageBox.Show(ex.StackTrace);
+          
+                
+            }
+        }
+    }
+
+    public class AuthResult {
+        [JsonProperty("token_type")]
+        public string TokenType;
+
+        [JsonProperty("state")]
+        public string State;
+
+        [JsonProperty("scope")]
+        public string Scope;
+
+        [JsonProperty("refresh_token")]
+        public string RefreshToken;
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn;
+
+        [JsonProperty("access_token")]
+        public string AccessToken;
+    }
+
+    public class RootAuthApi {
+        [JsonProperty("jsonrpc")]
+        public string Jsonrpc;
+
+        [JsonProperty("result")]
+        public AuthResult Result;
+
+        [JsonProperty("usIn")]
+        public long UsIn;
+
+        [JsonProperty("usOut")]
+        public long UsOut;
+
+        [JsonProperty("usDiff")]
+        public int UsDiff;
+
+        [JsonProperty("testnet")]
+        public bool Testnet;
+    }
+
+
+
+    public class ForeignJsonNetContainerConverter : ForeignJsonNetBaseConverter {
+        static readonly string[] Names = new[]
+        {
+        "Newtonsoft.Json.Linq.JObject",
+        "Newtonsoft.Json.Linq.JArray",
+        "Newtonsoft.Json.Linq.JConstructor",
+        "Newtonsoft.Json.Linq.JRaw",
+    };
+
+        protected override IReadOnlyCollection<string> TypeNames { get { return Names; } }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+            var json = value.ToString();
+            // Fix indentation
+            using (var stringReader = new StringReader(json))
+            using (var jsonReader = new JsonTextReader(stringReader)) {
+                writer.WriteToken(jsonReader);
+            }
+        }
+    }
+
+    public class ForeignJsonNetValueConverter : ForeignJsonNetBaseConverter {
+        static readonly string[] Names = new[]
+        {
+        "Newtonsoft.Json.Linq.JValue",
+    };
+
+        protected override IReadOnlyCollection<string> TypeNames { get { return Names; } }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+            var underlyingValue = ((dynamic)value).Value;
+            if (underlyingValue == null) {
+                writer.WriteNull();
+            } else {
+                // JValue.ToString() will be wrong for DateTime objects, we need to serialize them instead.
+                serializer.Serialize(writer, underlyingValue);
+            }
+        }
+    }
+
+    public abstract class ForeignJsonNetBaseConverter : Newtonsoft.Json.JsonConverter {
+        protected abstract IReadOnlyCollection<string> TypeNames { get; }
+
+        public override bool CanConvert(Type objectType) {
+            if (objectType.IsPrimitive)
+                return false;
+            // Do not use the converter for Native JToken types, only non-native types with the same name(s).
+            if (objectType == typeof(JToken) || objectType.IsSubclassOf(typeof(JToken)))
+                return false;
+            var fullname = objectType.FullName;
+            if (TypeNames.Contains(fullname))
+                return true;
+            return false;
+        }
+
+        public override bool CanRead { get { return false; } }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+            throw new NotImplementedException();
+        }
+    }
+
 }
